08adc019ae92bc7a2cf0a3b8ae5b0a2319ed0daf
miaotony <41962043+miaotony@users.noreply.github.com>
2019-12-15 04:44:30 +0800

    注释掉tkinter的语句

--- getClassSchedule.py
+++ getClassSchedule.py
@@ -14,8 +14,8 @@ import time
 import random
 import json
 import logging
-import tkinter as tk
-import tkinter.ttk
+# import tkinter as tk
+# import tkinter.ttk
 from PIL import Image, ImageTk
 from io import BytesIO
 # from pytesseract import image_to_string
